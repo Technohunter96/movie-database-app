701562c62bc9be8fcf15aa683a1ba8388b4d648a
Technohunter96 <129107751+Technohunter96@users.noreply.github.com>
2024-01-07 20:41:42 +0100

    commit

--- src/pages/Kids.jsx
+++ src/pages/Kids.jsx
@@ -1,34 +1,27 @@
-import React, { useEffect, useContext } from 'react';
-import MovieList from "../components/MovieList";
-import MovieContext from '../context/MovieContext';
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import React, { useEffect, useContext } from "react"
+import MovieList from "../components/MovieList"
+import MovieContext from "../context/MovieContext"
+import axios from "axios"
 
 function Kids() {
-   const { setMovies, setLoading, apiUrl, page, setHasMore } = useContext(MovieContext);
-   
+   const { setMovies, setLoading, apiUrl, page } = useContext(MovieContext)
+
    useEffect(() => {
       const getMovies = async () => {
          try {
             const response = await axios.get(`${apiUrl}&page=${page}`)
-            const { data } = response;
-            if (!data.results || data.results.length === 0) {
-               setHasMore(false);
-               setLoading(false);
-               toast.error("No results in current category");
-            } else {
-               setMovies(data.results);
-               setLoading(false);
-            }
+            const { data } = response
+            setMovies(data.results)
+            setLoading(false)
          } catch (error) {
-            console.log(error);
-            setLoading(false);
+            console.log(error)
+            setLoading(false)
          }
-      };
-      getMovies();
-   }, [setMovies, setLoading, apiUrl, page, setHasMore]);
-   
-   return <MovieList />;
+      }
+      getMovies()
+   }, [setMovies, setLoading, apiUrl, page, setHasMore])
+
+   return <MovieList />
 }
 
-export default Kids;
+export default Kids
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useContext } from 'react';
-import MovieList from "../components/MovieList";
-import MovieContext from '../context/MovieContext';
-import axios from 'axios';
+import React, { useEffect, useContext } from "react"
+import MovieList from "../components/MovieList"
+import MovieContext from "../context/MovieContext"
+import axios from "axios"
 
 function Movies() {
-   const { setMovies, setLoading, apiUrl, page } = useContext(MovieContext);
-   
+   const { setMovies, setLoading, apiUrl, page } = useContext(MovieContext)
+
    useEffect(() => {
       const getMovies = async () => {
          try {
             const response = await axios.get(`${apiUrl}&page=${page}`)
-            const { data } = response;
-            setMovies(data.results);
-            setLoading(false);
+            const { data } = response
+            setMovies(data.results)
+            setLoading(false)
          } catch (error) {
-            console.log(error);
-            setLoading(false);
+            console.log(error)
+            setLoading(false)
          }
-      };
-      getMovies();
-   }, [setMovies, setLoading, apiUrl, page]);
-   
-   return <MovieList />;
+      }
+      getMovies()
+   }, [setMovies, setLoading, apiUrl, page])
+
+   return <MovieList />
 }
 
-
-export default Movies;
+export default Movies
